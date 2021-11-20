fa0f51256f7f375bb0d06c7f317ca0c07dbf4f68
Maksim Lesnikov <maxim-borzya75@gmail.com>
2021-11-20 20:41:49 +0700

    В DeviceTest.py добавил сравнение введеной строки с тем, что пришло в девайс

--- ДЗ 3 урок/devices.tests/DeviceTest.py
+++ ДЗ 3 урок/devices.tests/DeviceTest.py
@@ -21,11 +21,12 @@ class DeviceTestCase(unittest.TestCase):
         self.assertEqual('line_1', read_line(open_device('/devices/dev4')))
 
     def test_write_line(self):
-        self.assertRaises(PermissionError, write_line, open_device('/devices/dev0'), 'VOLGA')
+        self.assertRaises(PermissionError, write_line, open_device('/devices/dev0'),'VOLGA')
         self.assertRaises(IOError, write_line, open_device('/devices/dev4'), '70RUS')
         word: Device = open_device('/devices/dev3')
         self.assertEqual('1', read_line(word))
-        write_line(word, 'TEst')
+        write_line(word, 'Test')
+        self.assertEqual('1', read_line(open_device('/devices/dev3')), 'Test')
 
 
 if __name__ == '__main__':
