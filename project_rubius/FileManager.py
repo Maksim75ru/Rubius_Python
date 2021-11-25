65565a9d0575541f5a9ca7365edf5e489f0758a0
Maksim <maxim-borzya@mail.ru>
2021-11-25 15:53:51 +0700

    Добавил def __init__, @staticmethod

--- project_rubius/FileManager.py
+++ project_rubius/FileManager.py
@@ -1,34 +1,44 @@
+from typing import Dict
 from dataclasses import dataclass
-from typing import Dict, Iterable
-
 
 @dataclass
-class File:
-    id: int
-    author: str
-    data: Iterable[str]
+class File:  # Класс это некоторое описание типа. Этот класс объединяет функции, которые ссылаются на него. Далее мы можем порождать экземпляры этого класса. Класс содержит набор связанных с ним функций.
+    def __init__(self, file_id: int, author: str, data: str):  # self в скобка в начале - это некое общее наименование. Функция init позволяет правильно проинициализировать содержимое класса.
+        self.id = file_id  # self указывает на то, что мы присваиваем id конкретно своему полю id. Так же это означает принадлежность к какому-то объекту.
+        self.author = author
+        self.data = data
+
+# Функция находящаяся внутри класса называется метод.
+    def get_data(self) -> str:  # Ссылка на текущий объект.
+        return self.data
+
+    @staticmethod  # Чтобы можно было легко сослаться. Например: print(file.get_all_files())
+    def get_all_files():
+        return LIST_OF_FILES.values()  # Метод .values() - вернуть все значения
+
+    @staticmethod
+    def add_file(file):  # Тут указали только server, т.к. по дефолту знаем свой объект. (server: File) В скобках указываем то, что функция должная принять на вход. server - это своего рода ссылка на собственный объект. File - это подсказка на тип объекта.
+        LIST_OF_FILES[file.id] = file  # [server.id], id - is key
+
+    @staticmethod
+    def remove_file(file_id):  # file_id - назвали так, потому что в Python есть встроенная функция id/ Поэтому file_id
+        del LIST_OF_FILES[file_id]  # del is key word
 
 
 LIST_OF_FILES: Dict[int, File] = {}  # Dict[int, File] - указали, что Dict у нас от int и от File
 
 
-def get_all_files() -> Iterable[File]:
-    return LIST_OF_FILES.values()  # Метод .values() - вернуть все значения
-
-
-def add_file(server: File):  # В скобках указываем то, что функция должная принять на вход
-    LIST_OF_FILES[server.id] = server  # [server.id], id - is key
-
-
-def remove_file(file_id: int):  # file_id - назвали так, потому что в Python есть встроенная функция id/ Поэтому file_id
-    del LIST_OF_FILES[file_id]  # del is key word
-
-
 if __name__ == '__main__':
-    add_file(File(0, "user_1", "file_1"))
-    add_file(File(1, "user_1", "file_2"))
+    # File(0, "user_1", "file_1")
+    # File(1, "user_1", "file_2")
+    #
+    # print(get_all_files())
+    File.add_file(File(0, "user_1", "file_1"))
+    File.add_file(File(1, "user_2", "file_2"))
+    file = File(1, "user_1", "file_2")
+    print(file.get_data())  # file.get_data() file - означает объект который мы вызываем, выше в классе он обозначается как self. Функция get_data, которая находится внутри класса называется МЕТОД.
+    print(File.get_all_files())
 
-    print(get_all_files())
 
-    remove_file(0)
-    print(get_all_files())
+    File.remove_file(0)
+    print(File.get_all_files())
