e5e4f3f16bea01f0a828e66b978c31a47544bacf
Maksim <maxim-borzya@mail.ru>
2021-11-25 12:30:34 +0700

    Закоментировал для понимания

--- project_rubius/FileManager.py
+++ project_rubius/FileManager.py
@@ -9,19 +9,19 @@ class File:
     data: Iterable[str]
 
 
-LIST_OF_FILES: Dict[int, File] = {}
+LIST_OF_FILES: Dict[int, File] = {}  # Dict[int, File] - указали, что Dict у нас от int и от File
 
 
 def get_all_files() -> Iterable[File]:
-    return LIST_OF_FILES.values()
+    return LIST_OF_FILES.values()  # Метод .values() - вернуть все значения
 
 
-def add_file(server: File):
-    LIST_OF_FILES[server.id] = server
+def add_file(server: File):  # В скобках указываем то, что функция должная принять
+    LIST_OF_FILES[server.id] = server  # [server.id], id - is key
 
 
-def remove_file(file_id: int):
-    del LIST_OF_FILES[file_id]
+def remove_file(file_id: int):  # file_id - назвали так, потому что в Python есть встроенная функция id/ Поэтому file_id
+    del LIST_OF_FILES[file_id]  # del is key word
 
 
 if __name__ == '__main__':
