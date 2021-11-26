5e5e47e9d0995311e722d9bb6765213541a09730
Maksim <maxim-borzya@mail.ru>
2021-11-26 11:36:43 +0700

    Создал class FileManager

--- project_rubius/FileManager.py
+++ project_rubius/FileManager.py
@@ -1,42 +1,55 @@
-from typing import Dict
-
+# Тут я храню данные
 class File:  # Класс это некоторое описание типа. Этот класс объединяет функции, которые ссылаются на него. Далее мы можем порождать экземпляры этого класса. Класс содержит набор связанных с ним функций.
-    def __init__(self, file_id: int, author: str, data: str):  # self в скобка в начале - это некое общее наименование. Функция init позволяет правильно проинициализировать содержимое класса.
-        self.id = file_id  # self указывает на то, что мы присваиваем id конкретно своему полю id. Так же это означает принадлежность к какому-то объекту.
-        self.author = author
-        self.data = data
 
-# Функция находящаяся внутри класса называется метод.
-    def get_data(self) -> str:  # Ссылка на текущий объект.
-        return self.data
+    def __init__(self, file_id: int, author: str, data: str):
+        self.__disk = {
+            'id': file_id,
+            'author': author,
+            'data': data,
+        }
 
-    @staticmethod  # Чтобы можно было легко сослаться. Например: print(file.get_all_files())
-    def get_all_files():
-        return LIST_OF_FILES.values()  # Метод .values() - вернуть все значения
+    def __repr__(self) -> str:
+        return f'File(id={self.id}, author={self.__disk["author"]}, data={self.__disk})'
 
-    @staticmethod
-    def add_file(file):  # Тут указали только server, т.к. по дефолту знаем свой объект. (server: File) В скобках указываем то, что функция должная принять на вход. server - это своего рода ссылка на собственный объект. File - это подсказка на тип объекта.
-        LIST_OF_FILES[file.id] = file  # [server.id], id - is key
+    @property
+    def id(self) -> int:
+        return self.__disk['id']
 
-    @staticmethod
-    def remove_file(file_id):  # file_id - назвали так, потому что в Python есть встроенная функция id/ Поэтому file_id
-        del LIST_OF_FILES[file_id]  # del is key word
+    @property
+    def data(self) -> str:
+        return self.__disk['data']
 
+    @data.setter
+    def data(self, data: str):
+        self.__disk['data'] = data
 
-LIST_OF_FILES: Dict[int, File] = {}  # Dict[int, File] - указали, что Dict у нас от int и от File
+# Этот класс занимается управлением файлами(добавить, удалить и т.д.).
+class FileManager:
+    def __init__(self, name: str):
+        self.__name = name
+        self.__files = {}
+
+    def get_all_files(self):
+        return self.__files.values()  # Метод .values() - вернуть все значения
+
+    def add_file(self,
+                 file):  # Тут указали только server, т.к. по дефолту знаем свой объект. (server: File) В скобках указываем то, что функция должная принять на вход. server - это своего рода ссылка на собственный объект. File - это подсказка на тип объекта.
+        self.__files[file.id] = file  # [server.id], id - is key
+
+    def remove_file(self,
+                    file_id):  # file_id - назвали так, потому что в Python есть встроенная функция id/ Поэтому file_id
+        del self.__files[file_id]  # del is key word
 
 
 if __name__ == '__main__':
-    # File(0, "user_1", "file_1")
-    # File(1, "user_1", "file_2")
-    #
-    # print(get_all_files())
-    File.add_file(File(0, "user_1", "file_1"))
-    File.add_file(File(1, "user_2", "file_2"))
+    personal_file_manager = FileManager('personal')
+    personal_file_manager.add_file(File(0, "user_1", "file_1"))
+    personal_file_manager.add_file(File(1, "user_2", "file_2"))
+    print(personal_file_manager.get_all_files())
+
+    work_files_manager = FileManager('work')
+
     file = File(1, "user_1", "file_2")
-    print(file.get_data())  # file.get_data() file - означает объект который мы вызываем, выше в классе он обозначается как self. Функция get_data, которая находится внутри класса называется МЕТОД.
-    print(File.get_all_files())
-
-
-    File.remove_file(0)
-    print(File.get_all_files())
+    print(file.data)
+    work_files_manager.add_file(file)
+    print(work_files_manager.get_all_files())
